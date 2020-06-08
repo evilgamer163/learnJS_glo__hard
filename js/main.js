fc0b3b28e4cfc2d768636c65fcced0bd931e2439
evilgamer163 <evilgamestm@gmail.com>
2020-06-08 09:27:04 +0400

    Правки:)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 const result = document.querySelector('.result');
 
 let num = 266219,
-    numArr = num.toString().split('');
+    numArr = num.toString().split(''),
+    res = 1;
 
 console.log(numArr);
-
-let res = 1;
 
 for (let i = 0; i < numArr.length; i++) {
     res *= numArr[i];
 }
 
-res = res ** 3;
+res **= 3;
 console.log(res);
 
 result.textContent += res.toString().slice(0, 2);
-
