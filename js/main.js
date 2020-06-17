8386a901aeab7a712216d0d5c0118a27fa432b91
evilgamer163 <evilgamestm@gmail.com>
2020-06-17 11:17:37 +0400

    Предварительная версия

--- js/main.js
+++ js/main.js
@@ -1,56 +1,13 @@
-'use strict'
+'use strict';
 
-//Переменные
-let lang = '',
-    ruDaysWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    enDaysWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Fridey', 'Saturday', 'Sunday'],
-    weeksArr = {
-        'ru' : ruDaysWeek,
-        'en' : enDaysWeek
-    },
-    namePerson = '';
+const today = document.querySelector('.today'),
+    days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда',
+            'Четверг', 'Пятница', 'Суббота'],
+    months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 
+            'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'],
+    date = new Date(),
+    day = date.getDay(),
+    year = date.getFullYear() + ' года';
 
-lang = prompt('Введите значение переменной "lang" (ru/en)');
-
-//Функция перебора массива
-const sortDay = (arr) => {
-    arr.forEach(item => {
-        console.log(item);
-    });
-};
-
-
-//<--- Задание № 1 --->
-//if
-if(lang === 'ru') {
-    sortDay(ruDaysWeek);
-} else if(lang === 'en') {
-    sortDay(enDaysWeek);
-} else {
-    lang = prompt('Введите корректное значение! (ru/en)');
-}
-
-//switch-case
-switch(lang) {
-    case 'ru':
-        sortDay(ruDaysWeek);
-        break;
-    case 'en':
-        sortDay(enDaysWeek);
-        break;
-    default:
-        lang = prompt('Введите корректное значение! (ru/en)');
-        break;
-}
-
-//Многомерный массив
-console.log(String(weeksArr[lang]));
-
-
-//<--- Задание № 2 --->
-namePerson = prompt('Введите имя:');
-
-let position = (namePerson === 'Артем') ? 'директор' :
-                (namePerson === 'Максим') ? 'преподаватель' : 'студент';
-
-console.log(position);
+today.textContent = `Сегодня ${days[day]}, ${day} ${months[date.getMonth()]} 
+                ${year}, `;
